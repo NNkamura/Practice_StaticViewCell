f368b53a2455bfa38d9c91f24d29ffcaede0df4f
州 <>
2019-10-23 23:28:25 +0900

    正しくはStaticCellsのようなので修正。

--- Practice_StaticViewCell/Practice_StaticViewCell/TableViewController_1.swift
+++ Practice_StaticViewCell/Practice_StaticViewCell/TableViewController_1.swift
@@ -17,15 +17,15 @@ class TableViewController_1: UITableViewController {
 //        self.tableView.dataSource = self
     }
     
-//    // MARK: - Table view data source
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 1
-//    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
 }
